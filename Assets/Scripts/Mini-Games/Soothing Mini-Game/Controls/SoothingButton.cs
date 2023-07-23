f892968347bdfc05cab643fd426708f02c4e9df6
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-07-24 02:33:36 +0800

    Added Audio for Vignette and Static Effect

--- Assets/Scripts/Mini-Games/Soothing Mini-Game/Controls/SoothingButton.cs
+++ Assets/Scripts/Mini-Games/Soothing Mini-Game/Controls/SoothingButton.cs
@@ -6,9 +6,11 @@ public class SoothingButton : MonoBehaviour
     [Header("Properties")]
     [SerializeField] private float increaseAmount = 1f;
 
-    public void OnSootingButtonPressed()
+    public void OnSoothingButtonPressed()
     {
         SoothingMiniGame.Instance.IncreaseStabilizeBarValue(increaseAmount);
+
+        AudioManager.Instance.PlayOneShot("Circle Game Sound");
 
         transform.DOScale(1.5f, 0.25f).SetEase(Ease.OutBounce).OnComplete(() => transform.DOScale(1f, 0.25f));
 
